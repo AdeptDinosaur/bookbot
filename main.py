324b834de6fdd2eddfeeff84f48f56347d5ed3bb
adeptdinosaur <adeptdinosaur@gmail.com>
2025-05-21 21:25:24 -0400

    independent refactor

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
-        file_contents = f.read()
-        return file_contents
+        return f.read()
+        
 
-def book_words(get_book_text, path_to_file):
-    words = get_book_text(path_to_file).split()
+def book_words(text):
+    words = text.split()
     num_words = len(words)
     print(f"{num_words} words found in the document")
 
 def main():
-    book_words(get_book_text, "./books/frankenstein.txt")
-    
+    text = get_book_text("books/frankenstein.txt")
+    book_words(text)
 
 main()
