aa91c0dcda5911306609ef4e4569f6575fc90295
adeptdinosaur <adeptdinosaur@gmail.com>
2025-05-22 12:00:46 -0400

    bootdev refactor

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
-        
-
-def book_words(text):
-    words = text.split()
-    num_words = len(words)
-    print(f"{num_words} words found in the document")
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    book_words(text)
+    get_num_words(text)
 
 main()
