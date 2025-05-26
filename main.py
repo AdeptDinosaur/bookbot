3e78ea2c0c1be52552ef076e0ad7ba750cb1af69
adeptdinosaur <adeptdinosaur@gmail.com>
2025-05-26 12:53:30 -0400

    multiple book capability

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from stats import get_num_words
 from stats import get_num_characters
 from stats import get_list_dict
 from stats import sort_on
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(book_path)
     get_num_words(text)
     print("--------- Character Count -------")
     original_dict = get_num_characters(text)
